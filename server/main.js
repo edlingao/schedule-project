b4c9646e7981dbcce47af417ce5c456c10557d2f
edlingao <sonido244@gmail.com>
2021-02-24 02:11:04 -0600

    [main] First calendar structure

--- server/main.js
+++ server/main.js
@@ -1,8 +1,10 @@
-const express = require('express')
+import express from 'express'
+import path from 'path'
 const app = express()
 const port = 3000
-const path = require('path')
+const __dirname = path.resolve();
 
+app.use(express.static(`${__dirname}/dist`));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/dist/index.html`))
