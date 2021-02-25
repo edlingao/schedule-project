7ccaf60401849599b363018dd3a8e57d34ef0484
Edwin Iram Linares Gaona <sonido244@gmail.com>
2021-02-24 18:10:06 -0600

    [main] Main connection with the local mongo and examples of inserting on it

--- server/main.js
+++ server/main.js
@@ -1,13 +1,39 @@
 import express from 'express'
 import path from 'path'
+import { default as mongodb } from 'mongodb'
+const { MongoClient } = mongodb
+
 const app = express()
 const port = 3000
-const __dirname = path.resolve();
+const __dirname = path.resolve()
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.json());
+app.use(express.static(`${__dirname}/dist`))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/dist/index.html`))
+})
+MongoClient.connect('mongodb://localhost:27017', (err, db) => {
+  if(err){
+    throw err
+  }
+  const document = db.db('schedule');
+  const schedule = document.collection('activities')
+  app.get('/schedule', async (req, res) => {
+    const data = await schedule.find().toArray()
+    res.json(data)
+  });
+  app.post('/schedule', async (req, res) => {
+    const newSchedule = req.body
+    let response = await schedule.insertOne(newSchedule)
+    response = !response.result.ok == 1 ? {status: false} : response
+    res.json(response)
+    // console.log( response )
+  })
+  app.delete('/schedule/:id', async (req, res) => {
+    const response = await schedule.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+    res.json(response);
+  })
 })
 
 app.listen(port, () => {
