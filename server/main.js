b96bd466d6a4ed83720248f39593f2ae60bdd427
edlingao <sonido244@gmail.com>
2021-02-28 18:03:00 -0600

    Destroying routes that help me load the ssl files

--- server/main.js
+++ server/main.js
@@ -22,9 +22,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/dist`))
 app.use(helment())
 
-app.get('/.well-known/pki-validation/40C952D3A11BAEC7CBF0A5325468A4BD.txt', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/40C952D3A11BAEC7CBF0A5325468A4BD.txt`))
-})
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/dist/index.html`))
 })
